ba346d0a1102972d95e7ef99fb4cb144928f6415
LeoRousseau <apb.leorousseau@gmail.com>
2024-01-29 18:34:31 -0500

    Fix icons

---
+++ src/UI/components/icons/buildings.tsx
@@ -0,0 +1,15 @@
+export const BuildingsIcon = (
+  <svg
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" />
+    <path d="M6 7h12a2 2 0 012 2v9a1 1 0 01-1 1h-2a1 1 0 01-1-1v-2a4 4 0 00-8 0v2a1 1 0 01-1 1H5a1 1 0 01-1-1V9a2 2 0 012-2" />
+  </svg>
+);
--- src/UI/components/settings/settingsContainer.tsx
+++ src/UI/components/settings/settingsContainer.tsx
@@ -1,11 +1,15 @@
 import "./settingsContainer.css";
 import { Tabs } from "../tabs/tabs";
 import { LayerIcon } from "../icons/layer";
+import { WaterIcon } from "../icons/water";
+import { LegendIcon } from "../icons/legend";
+import { BuildingsIcon } from "../icons/buildings";
 
 const tabData = [
   { label: "Layers", Icon: LayerIcon },
-  { label: "Water", Icon: LayerIcon },
-  { label: "Humans", Icon: LayerIcon },
+  { label: "Water", Icon: WaterIcon },
+  { label: "Buildings", Icon: BuildingsIcon },
+  { label: "Legend", Icon: LegendIcon },
 ];
 
 export function SettingsContainer() {
