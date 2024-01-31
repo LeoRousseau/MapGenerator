6f4a778a1355343f021cdf107963c4877d5e3809
LeoRousseau <apb.leorousseau@gmail.com>
2024-01-30 22:30:38 -0500

    Add viewbox

--- src/map/drawer/renderer.ts
+++ src/map/drawer/renderer.ts
@@ -1,26 +1,26 @@
 import { SVG, Svg } from "@svgdotjs/svg.js";
 
-const divID = "#canvas" ;
+const divID = "#canvas";
 let mapSVG: Svg;
 const size = {
-    width: 100,
-    height: 100
-}
+  width: 100,
+  height: 100,
+};
 
 export function getDimension() {
-    return size;
+  return size;
 }
 
 export function clear() {
-    mapSVG?.clear();
+  mapSVG?.clear();
 }
 
 export function initialize(width: number, height: number) {
-    size.width = width;
-    size.height = height;
-    mapSVG = SVG().addTo(divID).size(width, height);
+  size.width = width;
+  size.height = height;
+  mapSVG = SVG().addTo(divID).viewbox(0, 0, width, height).size(800, 800);
 }
 
 export function getSVG(): Svg {
-    return mapSVG;
+  return mapSVG;
 }
