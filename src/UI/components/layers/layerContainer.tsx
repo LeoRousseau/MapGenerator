92ad50a368b816d400296c4819b907835f3a9444
LeoRousseau <apb.leorousseau@gmail.com>
2024-01-30 22:21:06 -0500

    Add and delete layers

--- src/UI/components/layers/layerContainer.tsx
+++ src/UI/components/layers/layerContainer.tsx
@@ -1,26 +1,44 @@
-import { Layer, LayerProps } from "./layer";
-import './layerContainer.css'
+import { useState } from "react";
+import { Layer } from "./layer";
+import "./layerContainer.css";
 
-const layers: LayerProps[] = [
+const defautLayers = [
   {
     elevation: 0,
     color: "#CDCF6A",
     strokeThickness: 1,
   },
-  {
-    elevation: 100,
-    color: "#9AB875",
-    strokeThickness: 0,
-  }
 ];
 
 export const LayerContainer = () => {
+  const [layers, setLayers] = useState(defautLayers);
+
+  const deleteLayer = (index: number) => {
+    const newState = [...layers];
+    newState.splice(index, 1);
+    setLayers(newState);
+  };
+
+  const addLayer = () => {
+    if (layers.length >= 6) return;
+    const newState = [...layers];
+    newState.push({
+      elevation: 0,
+      color: "#CDCF6A",
+      strokeThickness: 0,
+    });
+    setLayers(newState);
+  };
+
   return (
     <div className="layer-container">
       <div className="layer-array">
-        {layers.map((l) => (
-          <Layer data={l}></Layer>
+        {layers.map((l, i) => (
+          <Layer data={{ ...l, onDelete: deleteLayer, index: i }} key={i}></Layer>
         ))}
+      </div>
+      <div className={layers.length < 6 ? "layer-add-button" : "layer-add-button disabled"} onClick={addLayer}>
+        Add Layer
       </div>
     </div>
   );
