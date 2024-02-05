a740f0544c0f281ea2c19e742acae7bfb3114bc6
LeoRousseau <apb.leorousseau@gmail.com>
2024-02-05 16:56:03 -0500

    Add download option

--- src/UI/components/topBar.tsx
+++ src/UI/components/topBar.tsx
@@ -1,6 +1,7 @@
 import "./topBar.css";
 import { SimpleButton } from "./simpleButton";
 import { generateMap } from "../../map/generateMap";
+import { downloadSVGAsText } from "../../map/download/download";
 
 export function TopBar() {
   return <div className="topbar-container">
@@ -9,5 +10,7 @@ export function TopBar() {
     <SimpleButton title="Apply settings" onClick={()=> console.log('clicked')}></SimpleButton>
     <div className="topbar-delimiter"></div>
     <SimpleButton title="Regenerate" onClick={()=> generateMap()}></SimpleButton>
+    <div className="topbar-delimiter"></div>
+    <SimpleButton title="Download" onClick={()=> downloadSVGAsText()}></SimpleButton>
   </div>;
 }
---
+++ src/map/download/download.ts
@@ -0,0 +1,11 @@
+import { getSVG } from "../drawer/renderer";
+
+export function downloadSVGAsText() {
+  const svg = getSVG().node;
+  const base64doc = btoa(unescape(encodeURIComponent(svg.outerHTML)));
+  const a = document.createElement("a");
+  const e = new MouseEvent("click");
+  a.download = "download.svg";
+  a.href = "data:image/svg+xml;base64," + base64doc;
+  a.dispatchEvent(e);
+}
