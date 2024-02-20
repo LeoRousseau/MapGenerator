0f4b401e1d701434dda7a30f0b089b7925532aeb
LeoRousseau <apb.leorousseau@gmail.com>
2024-02-19 19:18:00 -0500

    Improve thick lines

--- src/map/drawer/thickLine.ts
+++ src/map/drawer/thickLine.ts
@@ -13,14 +13,24 @@ export function createThickLine(points: Point3[]): Point2[] {
 }
 
 function computeSidePoints(current: Point3, prev?: Point3, next?: Point3): { left: Point2; right: Point2 } {
-  const w = current.z / 2;
-  const v1 = prev || current;
-  const v2 = next || current;
-  const dir = substract(v2, v1);
+  let w = current.z / 2;
+  let dir;
+  if (prev && next) {
+    const v1 = normalize(substract(current, prev));
+    const v2 = normalize(substract(next, current));
+    const angle = Math.acos(dot(v1, v2));
+    w *= Math.max(1, angle);
+    dir = add(v1, v2);
+  } else {
+    const v1 = prev || current;
+    const v2 = next || current;
+    dir = substract(v2, v1);
+  }
+
   const ndir = normalize(dir);
   const perp = getPerpendicular(ndir);
-  const p1 = add({ ...current }, scale(perp, w));
-  const p2 = substract({ ...current }, scale(perp, w));
+  const p1 = add(current, scale(perp, w));
+  const p2 = substract(current, scale(perp, w));
   return {
     left: p1,
     right: p2,
@@ -50,4 +60,8 @@ export function substract(v1: Point2, v2: Point2) {
 
 function scale(v1: Point2, s: number) {
   return { x: v1.x * s, y: v1.y * s };
+}
+
+function dot(v1: Point2, v2: Point2) {
+  return v1.x * v2.x + v1.y * v2.y;
 }
