6666adbc681769f80c8b6eb955d5c1549147aa31
LeoRousseau <apb.leorousseau@gmail.com>
2024-02-20 19:35:43 -0500

    Add random mounts

--- src/map/mount/mount.ts
+++ src/map/mount/mount.ts
@@ -2,6 +2,12 @@ import { NumberMap } from "../../types";
 import { getSVG } from "../drawer/renderer";
 import { getPeaks } from "./peaks";
 
+const paths = [
+  "M1 6L1.61538 4.42105L2.84615 2.28947L4 1L5.92308 1.47365L7 3.5L7.5 5L8 6",
+  "M1 7L1.61538 5.42105L2.84615 3.28947L4 1L4.92308 2.65789L5.92308 4.15789L6.76923 5.57895L8 7",
+  "M1 6.5L2 5L3 3L4 1L5.5 2L6.76923 4L7.5 5L8 6.5",
+];
+
 export function generateMounts(source: NumberMap, maxCount = 20) {
   const peaks = getPeaks(source);
   const sortedNodes = peaks.sort((a, b) => b.cellValue - a.cellValue).filter((n) => n.cellValue > 0.3);
@@ -10,8 +16,9 @@ export function generateMounts(source: NumberMap, maxCount = 20) {
 }
 
 function drawMount(node: { x: number; y: number }, step: number) {
+  const path = paths[Math.floor(Math.random() * 3)]
   getSVG()
-    .path("M1 7L1.61538 5.42105L2.84615 3.28947L4 1L4.92308 2.65789L5.92308 4.15789L6.76923 5.57895L8 7")
+    .path(path)
     .move(step * node.x, step * node.y)
     .fill("#00000000")
     .stroke({ color: "#54552b", width: 1.5, linecap: "round", linejoin: "round" });
