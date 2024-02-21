9b4dd03a433ed082f89a3605c6710a87d3cf03d3
LeoRousseau <apb.leorousseau@gmail.com>
2024-02-20 20:26:41 -0500

    Update mount

--- src/map/mount/mount.ts
+++ src/map/mount/mount.ts
@@ -8,9 +8,9 @@ const paths = [
   "M1 6.5L2 5L3 3L4 1L5.5 2L6.76923 4L7.5 5L8 6.5",
 ];
 
-export function generateMounts(source: NumberMap, maxCount = 20) {
+export function generateMounts(source: NumberMap, maxCount = 50) {
   const peaks = getPeaks(source);
-  const sortedNodes = peaks.sort((a, b) => b.cellValue - a.cellValue).filter((n) => n.cellValue > 0.3);
+  const sortedNodes = peaks.sort((a, b) => b.cellValue - a.cellValue).filter((n) => n.cellValue > 0.1);
   sortedNodes.length = Math.min(maxCount, sortedNodes.length);
   sortedNodes.forEach((n) => drawMount(n, 5));
 }
